e3553803efb7ba791d097d255bb8d376b1266a53
Gary Stringham <garystringham@aparium.com>
2025-02-10 11:24:53 -0800

    Commit

--- westland-01.js
+++ westland-01.js
@@ -1,71 +1,69 @@
-$(document).ready(function () {
-  function handleScroll() {
-    const scrollY = $(window).scrollTop();
-    if (scrollY > 0) {
-      $("body").addClass("scrolled");
-    } else {
-      $("body").removeClass("scrolled");
-    }
-  }
+$(document).ready(function() {
 
-  $(window).scroll(handleScroll);
-  handleScroll();
+	function handleScroll() {
+		const scrollY = $(window).scrollTop();
+		if (scrollY > 0) {
+			$('body').addClass('scrolled');
+		} else {
+			$('body').removeClass('scrolled');
+		}
+	}
 
-  $("head").append('<link rel="shortcut icon" href="https://cdn.shrglobal.com/IBE/CrsMedia/P17399/fav-01.svg"/>');
+	$(window).scroll(handleScroll);
+	handleScroll();
 
-  $("#trg").click(function () {
-    $("#ws-utility").toggleClass("open");
-    $("#headerwrapper").toggleClass("opened");
-    $("#ws-main").toggleClass("opened");
-    $("#ws-header").toggleClass("opened");
-    $(this).toggleClass("to-close");
-  });
+	$('head').append('<link rel="shortcut icon" href="https://cdn.shrglobal.com/IBE/CrsMedia/P17399/fav-01.svg"/>');
 
-  $("#trg-navigation").click(function () {
-    $("#site-navigation").toggleClass("opened");
-    $(this).toggleClass("closed");
-  });
+	$('#trg').click(function() {
+		$('#ws-utility').toggleClass('open');
+		$('#headerwrapper').toggleClass('opened');
+		$('#ws-main').toggleClass('opened');
+		$('#ws-header').toggleClass('opened');
+		$(this).toggleClass('to-close');
+	});
 
-  $("#mobile-close").click(function () {
-    $("#site-navigation").removeClass("opened");
-    $("#trg-navigation").removeClass("closed");
-  });
+	$('#trg-navigation').click(function() {
+		$('#site-navigation').toggleClass('opened');
+		$(this).toggleClass('closed');
+	});
 
-  const menuItems = document.querySelectorAll(".menu-item");
+	$('#mobile-close').click(function() {
+		$('#site-navigation').removeClass('opened');
+		$('#trg-navigation').removeClass('closed'); 
+	});
 
-  const addSubNav = (menuItem) => {
-    const hasChildren = menuItem.querySelector("ul");
-    if (hasChildren) {
-      menuItem.classList.add("has-child");
+	const menuItems = document.querySelectorAll('.menu-item');
 
-      const subNavBtn = document.createElement("button");
-      subNavBtn.classList.add("subnav-btn");
+	const addSubNav = (menuItem) => {
+		const hasChildren = menuItem.querySelector('ul');
+		if (hasChildren) {
+			menuItem.classList.add('has-child');
 
-      // Create the SVG element from the provided code
-      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      svg.setAttribute("viewBox", "0 0 40 21.44");
-      svg.setAttribute("width", "24");
-      svg.setAttribute("height", "24");
+			const subNavBtn = document.createElement('button');
+			subNavBtn.classList.add('subnav-btn');
 
-      // Add the provided SVG path
-      const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-      path.setAttribute("id", "Layer_20");
-      path.setAttribute(
-        "d",
-        "M18.99,21.02L.42,2.45C-.14,1.89-.14.99.41.43c0,0,0,0,0,0,.56-.56,1.46-.56,2.02,0,0,0,0,0,0,0l17.55,17.57L37.55.42c.56-.56,1.47-.56,2.03,0s.56,1.47,0,2.03l-18.56,18.56c-.56.56-1.46.56-2.02,0,0,0,0,0,0,0Z"
-      );
-      svg.appendChild(path);
+		      // Create the SVG element from the provided code
+			const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			svg.setAttribute('viewBox', '0 0 40 21.44');
+			svg.setAttribute('width', '24');
+			svg.setAttribute('height', '24');
 
-      subNavBtn.appendChild(svg);
-      menuItem.appendChild(subNavBtn);
+			    // Add the provided SVG path
+			const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+			path.setAttribute('id', 'Layer_20');
+			path.setAttribute('d', 'M18.99,21.02L.42,2.45C-.14,1.89-.14.99.41.43c0,0,0,0,0,0,.56-.56,1.46-.56,2.02,0,0,0,0,0,0,0l17.55,17.57L37.55.42c.56-.56,1.47-.56,2.03,0s.56,1.47,0,2.03l-18.56,18.56c-.56.56-1.46.56-2.02,0,0,0,0,0,0,0Z');
+			svg.appendChild(path);
 
-      subNavBtn.addEventListener("click", () => {
-        const subMenu = menuItem.querySelector(".menu-item-sub-menu");
-        subMenu.classList.toggle("opened");
-        menuItem.classList.toggle("opened");
-      });
-    }
-  };
+			subNavBtn.appendChild(svg);
+			menuItem.appendChild(subNavBtn);
 
-  menuItems.forEach(addSubNav);
+			subNavBtn.addEventListener('click', () => {
+				const subMenu = menuItem.querySelector('.menu-item-sub-menu');
+				subMenu.classList.toggle('opened');
+				menuItem.classList.toggle('opened');
+			});
+		}
+	};
+
+	menuItems.forEach(addSubNav);
 });
